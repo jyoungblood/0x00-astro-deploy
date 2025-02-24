a2cd87024e76f86b3f4a101e99a85994243a5f42
jonathan youngblood <jonathan.youngblood@gmail.com>
2025-02-24 13:47:40 -0600

    update rsync permissions

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ npm run build
 
 # add '-n' for dry run
 
-rsync -avhHP --delete-after --chmod=Du=rwx,Dg=rx,D=x,Fu=rwx,Fg=r,Fo=r --exclude '.htaccess' --exclude '.well-known' --exclude 'cgi-bin' $SITE_PATH -e "ssh -p $SSH_PORT" $SSH_USER@$SSH_SERVER:$DEPLOYMENT_PATH
+rsync -avhHP --delete-after --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --exclude '.htaccess' --exclude '.well-known' --exclude 'cgi-bin' $SITE_PATH -e "ssh -p $SSH_PORT" $SSH_USER@$SSH_SERVER:$DEPLOYMENT_PATH
 
 exit
